872bbc354abb4b4cfe750ff8bce57c98f8213085
cboenke <clara.boenke@web.de>
2021-02-17 14:48:47 +0100

    :recycle: Refactor code as preparation for API fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,43 @@ import Greeting from "../components/Greetings";
 import TrackItem from "../components/TrackItem";
 
 export default function Home() {
+  const tracks = [
+    {
+      imgSrc: "/img_track_1.jpg",
+      title: "Wer wenn nicht wir",
+      artist: "Wincent Weiss",
+    },
+    {
+      imgSrc: "/img_track_2.jpg",
+      title: "Found",
+      artist: "Dan Davidson",
+    },
+    {
+      imgSrc: "/img_track_3.jpg",
+      title: "Knockin' Boots",
+      artist: "Luke Bryan",
+    },
+    {
+      imgSrc: "/img_track_4.jpg",
+      title: "Corazón En La Maleta",
+      artist: "Luis Fonsi",
+    },
+    {
+      imgSrc: "/img_track_5.jpg",
+      title: "Savage Love (Laxed - Siren Beat)",
+      artist: "Jawsh 685, Jason Derulo",
+    },
+  ];
+
+  const trackItems = tracks.map((track) => (
+    <TrackItem
+      key={`${track.artist}_${track.title}`}
+      imgSrc={track.imgSrc}
+      title={track.title}
+      artist={track.artist}
+    />
+  ));
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,13 +50,7 @@ export default function Home() {
       Nukebox
       <Greeting name="Clara" />
       <main className={styles.main}>
-        <ul className={styles.tracklist}>
-          <TrackItem
-            imgSrc="/cover_track_1.jpg"
-            title="The Read"
-            artist="F Your Purse"
-          />
-        </ul>
+        <ul className={styles.tracklist}>{trackItems}</ul>
       </main>
     </div>
   );
