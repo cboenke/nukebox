3f8f7443277f199ff11749203cc9ad41c1954879
cboenke <clara.boenke@web.de>
2021-02-17 10:10:44 +0100

    :art: Improve integration of the cover image

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <main className={styles.main}>
         <ul className={styles.tracklist}>
           <TrackItem
-            cover="https://images.unsplash.com/photo-1566108254145-a8813d68d395?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
+            cover="/cover_track_1.jpg"
             artist="F Your Purse"
             title="The Read"
           />
