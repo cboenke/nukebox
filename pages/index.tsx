219692087a19de475c3cbd098ae5ec1fefad2b93
cboenke <clara.boenke@web.de>
2021-02-24 20:18:36 +0100

    ✨ Display favorite tracks on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Greeting from "../components/Greetings";
 import TrackItem from "../components/TrackItem";
+import useLocalStorage from "../hooks/useLocalStorage";
 import { useEffect, useState } from "react";
 import { APITrack, getTracks } from "../utils/api";
 import Link from "next/link";
 
 export default function Home() {
   const [tracks, setTracks] = useState<APITrack[]>([]);
+  const [favoriteSongs] = useLocalStorage<string[]>("favoriteSongs", []);
 
   useEffect(() => {
     console.log("Homepage is mounted");
@@ -16,17 +18,33 @@ export default function Home() {
     });
   }, []);
 
-  const trackItems = tracks.map((track) => (
-    <Link href={`/tracks/${track.id}`} key={track.id}>
-      <a>
-        <TrackItem
-          imgSrc={track.imgSrc}
-          title={track.title}
-          artist={track.artist}
-        />
-      </a>
-    </Link>
-  ));
+  const favoriteTrackItems = tracks
+    .filter((track) => favoriteSongs.includes(track.id))
+    .map((track) => (
+      <Link href={`/tracks/${track.id}`} key={track.id}>
+        <a>
+          <TrackItem
+            imgSrc={track.imgSrc}
+            title={track.title}
+            artist={track.artist}
+          />
+        </a>
+      </Link>
+    ));
+
+  const trackItems = tracks
+    .filter((track) => !favoriteSongs.includes(track.id))
+    .map((track) => (
+      <Link href={`/tracks/${track.id}`} key={track.id}>
+        <a>
+          <TrackItem
+            imgSrc={track.imgSrc}
+            title={track.title}
+            artist={track.artist}
+          />
+        </a>
+      </Link>
+    ));
 
   return (
     <div className={styles.container}>
@@ -37,6 +55,9 @@ export default function Home() {
       Nukebox
       <Greeting name="Clara" />
       <main className={styles.main}>
+        <p>Favoriten:</p>
+        <ul className={styles.tracklist}>{favoriteTrackItems}</ul>
+        <p>Auch super Songs:</p>
         <ul className={styles.tracklist}>{trackItems}</ul>
       </main>
     </div>
